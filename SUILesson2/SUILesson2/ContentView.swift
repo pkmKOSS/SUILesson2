69c766d64ed2eaf9db83980c4cb1bdd5cdea9baa
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2023-01-22 02:21:10 +0500

    SUILesson2Done - переделал проект.

--- SUILesson2/SUILesson2/ContentView.swift
+++ SUILesson2/SUILesson2/ContentView.swift
@@ -7,145 +7,302 @@
 
 import SwiftUI
 
+struct Bill {
+    let name: String
+    var value: Int
+}
+
 /// Представление с контентом приложения пикера
 struct ContentView: View {
 
-    // MARK: - private constants
-
+    // Constants
     private enum Constants {
-        static let timeText = "Время"
-        static let airModeText = "Авиарежим"
-        static let displayLightModeText = "Подсветка"
-        static let increaseBalanceText = "Пополнить баланс на: "
-        static let balanceIncreaseFor = "Ваш баланс пополнен на "
-        static let goodText = "Хорошо"
-        static let openBillText = "Открыть счет"
-        static let userfirstAndLastNameText = "ФИО держателя"
-        static let sendMoneyText = "Перевести деньги со счета на счет"
-        static let ketText = "Key"
-        static let alertMoneyTransitionText = "Какую сумму перевести?"
-        static let valueOfTransition = "Какую сумму перевести?"
-        static let alertMoneyTransitionQuestenTest = "Вы уверенны что хотите перевести: "
-        static let yesText = "Да"
-        static let debetBillText = "Дебетовый счет: "
-        static let creditBillText = "Кредитный счет: "
-        static let billsText = "Счета"
-        static let navigationTitleText = "Настройки"
-        static let fixedInputValue = 1000
+        static let debetCardIDName = "1234 4321 1423 2314"
+        static let creditCardIDName = "2314 4222 1423 4321"
+        static let emptyString = ""
+        static let showSettingsName = "Показать настройки"
+        static let addBillName = "arrowshape.turn.up.right"
+        static let walletName = "Кошелек"
+        static let wellcomeName = "Добро пожаловать!"
+        static let plusImageName = "plus"
+        static let debetCardName = "Дебетовая карта"
+        static let balanceName = "Баланс: "
+        static let rublesName = "руб."
+        static let addMoneyName = "Пополнить баланс"
+        static let valueOfMoneyName = "Сумма пополнения"
+        static let sendMoneyName = "Перевести деньги"
+        static let sendValueName = "Сумма перевода"
+        static let transferSuccessName = "Деньги переведены"
+        static let addingSuccessName = "Баланс успешно пополнен"
+        static let transferAlertName = "Вы уверенны что хотите перевести деньги?"
+        static let yesName = "Да"
+        static let noName = "Нет"
+        static let creditCardName = "Кредитная карта"
+        static let payCreditCardName = "Погасить долг"
+        static let payCreditCardValueName = "Сумма погашения"
+        static let payCreditCardAlertName = "Вы уверенны что хотите перевести деньги?"
+        static let verticalSpacing = 15
     }
 
     // MARK: - private properties
 
-    @State private var section = 0
-    @State private var cardsSection = 2
-    @State private var isOn = false
-    @State private var text = ""
-    @State private var isPresentedFirst = false
-    @State private var isPresentedSecond = false
-    @State private var isPresentedThree = false
-    @State private var isPresentedFour = false
-    @State private var debetCardBalance = 0
-    @State private var creditCardBalance = 50000
-    @State private var fixInputValue = 100
-    @State private var valueOfTransition = "0.00"
+    @State var isOnToggle = false
+    @State var isAddMoneyAlertShown = false
+    @State var isAddMoneySuccessfullyAlertShown = false
+    @State var isAddMoneyToCreditCardShown = false
+    @State var isSendMoneyToCreditShown = false
+    @State var isSendMoneyToDebetShown = false
+    @State var isSendMoneyAlertShown = false
+    @State var addMoneyText = ""
+    @State var sendToCreditMoneyText = ""
+    @State var sendToDebetMoneyText = ""
+    @State static var debetCardBalance = 37000
+    @State static var creditCardBalance = -50000
+    @State var debetCard = Bill(name: Constants.debetCardIDName, value: debetCardBalance)
+    @State var creditCard = Bill(name: Constants.creditCardIDName, value: creditCardBalance)
 
-    private var settingsTime = ["5 min", "10 min", "15 min"]
-    private var procents = ["5 процентов", "10 процентов", "15 процентов"]
-
-    // MARK: - public methods
+    // MARK: - public properties
 
     var body: some View {
-        NavigationStack {
-            Form {
-                makeTimePicker()
-                makeAviaModePicker()
-                makeLightModePicker()
-                makeIncreaseDebetValuePicker()
-                makeOpenBillButton()
-                makeMoneyTransitionButton()
-                makeBillsButton()
-            }
-        }.navigationBarTitle(Constants.navigationTitleText)
-    }
-
-    // MARK: - private methods
-
-    private func makeTimePicker() -> some View {
-        Picker(selection: $section) {
-            ForEach(0..<settingsTime.count) {
-                Text(self.settingsTime[$0])
-            }
-        } label: {
-            Text(Constants.timeText)
-        }.pickerStyle(.navigationLink)
-    }
-
-    private func makeAviaModePicker() -> some View {
-        Toggle(isOn: $isOn) {
-            Text(Constants.airModeText).foregroundColor(isOn ? Color.red : Color.gray)
-        }
-    }
-
-    private func makeLightModePicker() -> some View {
-        Picker(selection: $section) {
-            ForEach(0..<procents.count) {
-                Text(self.procents[$0])
-            }
-        } label: {
-            Text(Constants.displayLightModeText)
-        }
-    }
-
-    private func makeIncreaseDebetValuePicker() -> some View {
-        Button(action: {
-            self.debetCardBalance += Constants.fixedInputValue
-            self.isPresentedFirst = true
-        }, label: {
-            Text("\(Constants.increaseBalanceText)\(fixInputValue)")
-        }).confirmationDialog(Constants.ketText, isPresented: $isPresentedFirst) {
-            Button(Constants.goodText) {
-                print("\(Constants.increaseBalanceText)\(fixInputValue)")
-            }
-        } message: {
-            Text("\(Constants.balanceIncreaseFor)\(fixInputValue) и составляет \(debetCardBalance)")
-        }
-    }
-
-    private func makeOpenBillButton() -> some View {
-        Button(Constants.openBillText) {
-            self.isPresentedSecond = true
-        }.alert(Text(Constants.openBillText), isPresented: $isPresentedSecond) {
-            TextField(Constants.userfirstAndLastNameText, text: $text)
-        }
-    }
-
-    private func makeMoneyTransitionButton() -> some View {
-        Button(Constants.sendMoneyText) {
-            self.isPresentedThree = true
-        }.alert(Text(Constants.alertMoneyTransitionText), isPresented: $isPresentedThree) {
-            TextField(Constants.valueOfTransition, text: $valueOfTransition).keyboardType(.numberPad).alert(Text("\(Constants.alertMoneyTransitionQuestenTest)\(valueOfTransition)"), isPresented: $isPresentedFour) {
-                Button(Constants.yesText) {
-                    guard
-                        let value = Int(valueOfTransition)
-                    else {
-                        return
+        VStack {
+            ZStack {
+                HStack {
+                    VStack {
+                        walletView.frame(alignment: .center)
                     }
-                    debetCardBalance += value
-                    creditCardBalance -= value
+                    Spacer()
+                }
+                startScreenYellowView
+                wellcomeTextView
+            }
+            showSettingsToggleView
+        }.animation(.spring())
+    }
+
+    // MARK: - private properties
+
+    private var startScreenYellowView: some View {
+        RoundedRectangle(cornerRadius: 10).fill(Color.yellow).offset(x: isOnToggle ? 500 : 0)
+    }
+
+    private var showSettingsToggleView: some View {
+        Toggle(isOn: $isOnToggle) {
+            Text(Constants.showSettingsName)
+        }.padding()
+    }
+
+    private var wellcomeTextView: some View {
+        Text(Constants.wellcomeName)
+            .offset(x: isOnToggle ? 500 : 0)
+            .frame(width: 170, height: 30)
+            .background(Color.white)
+            .cornerRadius(10)
+    }
+
+    private var sendMoneyImage: some View {
+        Image(systemName: Constants.addBillName)
+            .frame(width: 40, height: 40)
+            .background(Color.yellow)
+            .foregroundColor(.white)
+            .cornerRadius(5)
+            .padding()
+    }
+
+    private var titleView: some View {
+        HStack {
+            Spacer(minLength: 125)
+
+            Text(Constants.walletName)
+                .cornerRadius(10)
+                .padding()
+                .font(.title)
+                .italic()
+
+            Spacer()
+
+            Button {
+
+            } label: {
+                Image(systemName: Constants.plusImageName)
+                    .frame(width: 30, height: 30)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+                    .padding()
+            }
+        }
+    }
+
+    private var debetCardView: some View {
+        VStack(spacing: 20) {
+            Text(Constants.debetCardName)
+            Text(debetCard.name)
+            HStack() {
+                Text(Constants.balanceName)
+                Text("\(debetCard.value)")
+                Text(Constants.rublesName)
+            }
+        }
+    }
+
+    private var addMoneyButtonView: some View {
+        Button(Constants.addMoneyName) {
+            self.isAddMoneyAlertShown = true
+        }
+        .alert(Constants.addMoneyName, isPresented: $isAddMoneyAlertShown, actions: {
+            TextField(Constants.valueOfMoneyName, text: Binding(get: {
+                self.addMoneyText
+            }, set: { newValue in
+                self.addMoneyText = newValue
+            }), onEditingChanged: { isEditingEnd in
+                guard
+                    !isEditingEnd,
+                    let money = Int(addMoneyText)
+                else { return }
+                self.debetCard.value += money
+                self.isAddMoneySuccessfullyAlertShown = true
+            })
+            .foregroundColor(.black)
+        })
+        .buttonStyle(BorderlessButtonStyle())
+        .padding()
+        .frame(width: 200, height: 40)
+        .background(Color.green)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+    }
+
+    private var sendMoneyToCreditCardButtonView: some View {
+        Button {
+            self.isSendMoneyToCreditShown = true
+        } label: {
+            sendMoneyImage
+        }
+        .alert(Constants.sendMoneyName, isPresented: $isSendMoneyToCreditShown, actions: {
+            TextField(Constants.sendValueName, text: Binding(get: {
+                self.sendToCreditMoneyText
+            }, set: { newValue in
+                self.sendToCreditMoneyText = newValue
+            }), onEditingChanged: { isEditingEnd in
+                guard
+                    !isEditingEnd,
+                    let money = Int(sendToCreditMoneyText)
+                else { return }
+                self.creditCard.value += money
+                self.debetCard.value -= money
+                self.isSendMoneyAlertShown = true
+            }).foregroundColor(.black)
+        })
+        .alert(Text(Constants.transferSuccessName), isPresented: $isSendMoneyAlertShown, actions: {
+            Text(Constants.transferSuccessName)
+        })
+        .padding()
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    private var creditCardVStack: some View {
+        VStack(spacing: 20) {
+            Text(Constants.creditCardName)
+            Text(creditCard.name)
+            HStack {
+                Text(Constants.balanceName)
+                Text("\(creditCard.value)")
+                Text(Constants.rublesName)
+            }
+        }
+    }
+
+    private var payCreditCardButtonView: some View {
+        Button(Constants.payCreditCardName) {
+            self.isAddMoneyToCreditCardShown = true
+        }.alert(Constants.payCreditCardName, isPresented: $isAddMoneyToCreditCardShown, actions: {
+            TextField(Constants.payCreditCardValueName, text: Binding(get: {
+                self.addMoneyText
+            }, set: { newValue in
+                self.addMoneyText = newValue
+            }), onEditingChanged: { isEditingEnd in
+                guard
+                    !isEditingEnd,
+                    let money = Int(addMoneyText),
+                    money <= debetCard.value
+                else { return }
+                self.creditCard.value += money
+                self.debetCard.value -= money
+            })
+            .foregroundColor(.black)
+
+        })
+        .frame(width: 200, height: 40)
+        .background(Color.green)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+    }
+
+
+    private var sendToDebetCardButtonView: some View {
+        Button {
+            self.isSendMoneyToDebetShown = true
+        } label: {
+            sendMoneyImage
+        }
+        .alert(Constants.sendMoneyName, isPresented: $isSendMoneyToDebetShown, actions: {
+            TextField(Constants.sendMoneyName, text: Binding(get: {
+                self.sendToDebetMoneyText
+            }, set: { newValue in
+                self.sendToDebetMoneyText = newValue
+            }), onEditingChanged: { isEditingEnd in
+                guard
+                    !isEditingEnd,
+                    let money = Int(sendToDebetMoneyText)
+                else { return }
+                self.debetCard.value += money
+                self.creditCard.value -= money
+                self.isSendMoneyAlertShown = true
+            })
+            .foregroundColor(.black)
+        }).alert(isPresented: $isSendMoneyAlertShown) {
+            Alert(
+                title: Text(Constants.transferAlertName),
+                primaryButton: .default(Text(Constants.yesName)),
+                secondaryButton: .default(Text(Constants.noName))
+            )
+        }
+        .padding()
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    private var walletView: some View {
+        VStack(alignment: .center) {
+            titleView
+            Form {
+                debetCardView
+                HStack {
+                    addMoneyButtonView
+                    sendMoneyToCreditCardButtonView
+
+                }.alert(Text(Constants.addingSuccessName), isPresented: $isAddMoneySuccessfullyAlertShown) {
+                    Text(Constants.addingSuccessName)
+                }.alert(isPresented: $isSendMoneyAlertShown) {
+                    Alert(
+                        title: Text(Constants.transferAlertName),
+                        primaryButton: .default(Text(Constants.yesName)),
+                        secondaryButton: .default(Text(Constants.noName))
+                    )
+                }
+            }
+
+            Form {
+                creditCardVStack
+
+                HStack {
+                    payCreditCardButtonView
+                    sendToDebetCardButtonView
                 }
             }
         }
     }
-
-    private func makeBillsButton() -> some View {
-        Picker(selection: $cardsSection) {
-            Text("\(Constants.debetBillText)\(debetCardBalance)")
-            Text("\(Constants.creditBillText)\(creditCardBalance)")
-        } label: {
-            Text(Constants.billsText)
-        }.pickerStyle(.navigationLink)
-    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
